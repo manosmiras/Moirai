1795da497a6461a7415bae1d222e82aa19c14713
Manos Miras <manosmiras@outlook.com>
2023-07-04 22:12:12 +0300

    feat: process input & apply color in while loop

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,16 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+
+
+void processInput(GLFWwindow* window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -19,12 +29,16 @@ int main() {
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
 	
 	while(!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+		
+		// Render commands here
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	
