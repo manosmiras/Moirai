e933cb5428f2a577b805a6f87741037dbe8a2544
Manos Miras <manosmiras@outlook.com>
2023-07-22 17:24:39 +0300

    feat: load shaders from files

---
+++ Shader.h
@@ -0,0 +1,114 @@
+﻿#pragma once
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <glad/glad.h>
+
+class Shader
+{
+public:
+    GLuint id;
+    Shader(const GLchar* vertex_path, const GLchar* fragment_path)
+    {
+        // Read shader contents
+        const std::string vertex_content = read_file_contents(vertex_path);
+        const char* vertex_code = vertex_content.c_str();
+        const std::string fragment_content = read_file_contents(fragment_path);
+        const char* fragment_code = fragment_content.c_str();
+        // Compile vertex shader
+        const auto vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vertex_code, nullptr);
+        glCompileShader(vertex);
+        check_compilation_errors(vertex, "VERTEX");
+        // Compile fragment Shader
+        const auto fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fragment_code, nullptr);
+        glCompileShader(fragment);
+        check_compilation_errors(fragment, "FRAGMENT");
+        // Shader Program
+        id = glCreateProgram();
+        glAttachShader(id, vertex);
+        glAttachShader(id, fragment);
+        glLinkProgram(id);
+        check_compilation_errors(id, "PROGRAM");
+        // Delete the shaders as they're linked into our program now and no longer necessary
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    
+    void use()
+    {
+        glUseProgram(id);
+    }
+
+    void delete_program()
+    {
+        glDeleteProgram(id);
+    }
+    
+    void set_bool(const std::string &name, const bool value) const
+    {         
+        glUniform1i(glGetUniformLocation(id, name.c_str()), static_cast<int>(value)); 
+    }
+    
+    void set_int(const std::string &name, const int value) const
+    { 
+        glUniform1i(glGetUniformLocation(id, name.c_str()), value); 
+    }
+    
+    void set_float(const std::string &name, const float value) const
+    { 
+        glUniform1f(glGetUniformLocation(id, name.c_str()), value); 
+    }
+private:
+    void check_compilation_errors(const GLuint shader, const std::string& type)
+    {
+        int success;
+        char info_log[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, nullptr, info_log);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << info_log << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, nullptr, info_log);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << info_log << std::endl;
+            }
+        }
+    }
+
+    std::string read_file_contents(const std::string& filepath)
+    {
+        std::string contents;
+        std::ifstream file;
+
+        file.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        try 
+        {
+            // open files
+            file.open(filepath);
+            std::stringstream stream;
+            // read file's buffer contents into streams
+            stream << file.rdbuf();
+            // close file handlers
+            file.close();
+            // convert stream into string
+            contents = stream.str();
+        }
+        catch (std::ifstream::failure& e)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+        }
+        
+        return contents;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include "Shader.h"
+
+void process_input(GLFWwindow* window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	const auto window = glfwCreateWindow(800, 600, "Moirai", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	gladLoadGL();
+	glViewport(0, 0, 800, 600);
+	
+	auto shader = Shader("shaders/shader.vert", "shaders/shader.frag");
+	
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+	   -0.5f, -0.5f, 0.0f,  // bottom left
+	   -0.5f,  0.5f, 0.0f   // top left 
+   };
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};  
+	
+	unsigned int vbo, vao, ebo;
+	
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);  
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+	glEnableVertexAttribArray(0);
+	
+	glBindVertexArray(vao);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Render loop
+	while(!glfwWindowShouldClose(window))
+	{
+		process_input(window);
+		
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.use();
+		
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &ebo);
+	shader.delete_program();
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
