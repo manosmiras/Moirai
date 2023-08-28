e7be08e44f5395079ced4e1ea2e3cf0019866595
Manos Miras <manosmiras@outlook.com>
2023-08-28 18:57:27 +0300

    chore: remove unused shader reference

--- Moirai/Headers/Renderer.h
+++ Moirai/Headers/Renderer.h
@@ -13,7 +13,6 @@ public:
     Renderer(Window* window, Camera* camera, UserInterface* userInterface);
     ~Renderer();
     std::unique_ptr<Shader> phongShader;
-    std::unique_ptr<Shader> lightSourceShader;
     std::map<GLuint, int> textures;
     unsigned int cubeVao, lightCubeVao, vbo;
     void Render(float deltaTime);
